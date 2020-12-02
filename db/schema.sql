19298cfb5e05d49b51a02245804911ad6ef0d1f3
Ness <johnsondavid1107@gmail.com>
2020-12-02 12:51:50 -0500

    created mysql database

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### SCHEMA
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO-INCREMENT,
+    burger_name VARCHAR (100),
+    devoured BOOLEAN 
+
+
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Avovado Supreme", false)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseanator", false)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Mac Miller", false)
