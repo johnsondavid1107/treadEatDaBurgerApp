efc3d08c8734dcd1bcc95bd9cf9aec9cfe7c7356
Ness <johnsondavid1107@gmail.com>
2020-12-03 16:35:02 -0500

    added a data value to the button

--- public/assets/js/burgerClient.js
+++ public/assets/js/burgerClient.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $.ajax("/burger", {
         type: "GET"
-    }).then(function(res){
+    }).then(function (res) {
         console.log(res)
         // console.log(res.allInfo[2].id)
         console.log(res.allInfo.length)
@@ -13,22 +13,27 @@ $(document).ready(function(){
 
         let allInfo = res.allInfo
 
-        for (let i = 0; i < allInfo.length; i++){
-           devourSection.append("<div class=row id="+i+"x><div class='col-md-9 text-center'><li class=show>"+allInfo[i].id+". "+allInfo[i].burger_name)
-           $("#" +i).append("<div class=col-md-3><button class='btn btn-primary'>" +"Devour")
+        for (let i = 0; i < allInfo.length; i++) {
+            devourSection.append("<div class=row id=" + i + "><div class='col-md-9 text-center'><li class=show>" + allInfo[i].id + ". " + allInfo[i].burger_name)
+            $("#" + i).append("<div class=col-md-3><button class='btn btn-primary'data="+i+">" + "Devour")
 
         }
 
 
-    
-       
+
+        $("#burgerBtn").on("click", function () {
+            console.log("This was clicked");
+        })
 
 
 
 
 
 
-        
+
+
+
+
     })
 
 
